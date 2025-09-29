6f105267eb7cd954c85e6b0ad8967a6e449b292f
Jigar Sojitra <jigar.s@squareinfosoft.com>
2025-09-29 21:34:00 +0530

    main changes

--- app/database.py
+++ app/database.py
@@ -3,7 +3,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=0,
+    connect_args={
+        "connect_timeout": 10,
+        "application_name": "jigar_api"
+    }
+)
 
 AuthSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
